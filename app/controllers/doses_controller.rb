891d339c90604f6daaa471fef5b2c222c8b097ae
Lukas Meißner <meissnerlukas@yahoo.de>
2019-11-14 15:50:46 +0100

    Controller and routes

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail_id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
